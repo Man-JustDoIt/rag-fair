9fc3f4c4960f536befebfa7d093cc4628361d510
Day Vinn <dvudimtsev@gmail.com>
2024-10-10 06:44:41 +0300

    Замена названия кнопки на старте

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ dp = Dispatcher()
 async def start(message: types.Message):
     webAppInfo = types.WebAppInfo(url=url)
     builder = ReplyKeyboardBuilder()
-    builder.add(types.KeyboardButton(text='Отправить данные', web_app=webAppInfo))
+    builder.add(types.KeyboardButton(text='Запустить приложение', web_app=webAppInfo))
 
     await message.answer(text='Привет!', reply_markup=builder.as_markup())
 
@@ -33,6 +33,7 @@ async def start(message: types.Message):
 @dp.message(F.content_type == ContentType.WEB_APP_DATA)
 async def parse_data(message: types.Message):
     data = json.loads(message.web_app_data.data)
+    print(data)
     await message.answer(f'<b>{data["title"]}</b>\n\n<code>{data["desc"]}</code>\n\n{data["text"]}',
                          parse_mode=ParseMode.HTML)
 
