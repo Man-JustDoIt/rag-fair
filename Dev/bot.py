9f4a091d74f09b37f7b763abbe86096bb4b83576
Day Vinn <dvudimtsev@gmail.com>
2024-11-04 17:27:46 +0300

    Добавлена возможность хранить SQl процедуры в одной файле yaml

---
+++ Dev/bot.py
@@ -0,0 +1,120 @@
+# todo  0. Скрыть токен бота    -   done
+# todo  1. Клавиша старт и помощь внизу бота и на главном меню  -   done
+# todo  2. Приветствие с ФИО    -   done
+# todo  3. Вывести информацию при нажатии кнопки "О проекте"    -   done
+# todo  4. Добавить БД - SQLight, создать таблицы, если необходимо  -   done
+# todo  5. Добавить в лог БД факт входа пользователя
+# todo      5.1 Проверить наличие пользователя в БД. Если нет, то записать  -   done
+# todo      5.1.1 Обеспечить хранение нескольких SQL скриптов в одном файле  -   done
+# todo      5.2 Записать факт входа в систему
+# todo      5.3 Вернуть дату и время последнего входа и текущий уровень доступа
+
+# todo  7. Только админам показывать кнопку "Добавить информацию"
+# todo  8. Научиться строить сайты в зависимости от полученных данных иб БД
+
+
+import asyncio
+import logging
+import json
+import os
+from dotenv import load_dotenv
+import pandas as pd
+
+
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.enums.content_type import ContentType
+from aiogram.filters import CommandStart
+from aiogram.enums.parse_mode import ParseMode
+
+from mquery import mquery
+
+
+class BC:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+
+logging.basicConfig(level=logging.INFO)
+load_dotenv()
+bot = Bot(os.getenv('bot_token'))
+
+url = "https://man-justdoit.github.io/rag-fair/index.html"
+dp = Dispatcher()
+
+
+
+def check_users(message: types.Message):
+    tg_login = message.from_user.username
+    tg_id = message.from_user.id
+    first_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+
+    params = [tg_id]
+    res = mquery('check_users', params)
+
+    if isinstance(res, pd.DataFrame) and res.empty:
+        params = [tg_id, tg_login, first_name, last_name]
+        if not mquery('add_users', params):
+            print(f'{BC.FAIL}Error:{BC.ENDC} Не удалось добавить пользователя {tg_login} в БД!')
+
+    return first_name
+
+# обработка команды /start от бота
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    first_name = check_users(message)
+    await message.answer(text=f'Привет {first_name} !', reply_markup=create_btn())
+
+
+# обработка команды /help от бота
+@dp.message(lambda message: message.text in ['/help', "О проекте"])
+async def process_start_command(message: types.Message) -> None:
+    msg = ('Приложение \n'
+           '1. Информирует о программе лояльности профсоюзной организации Сбера \n' 
+           '2. Дает возможность размещать собственные частные объявления')
+    await message.answer(msg)
+
+
+@dp.message(F.content_type == ContentType.WEB_APP_DATA)
+async def parse_data(message: types.Message):
+    data = json.loads(message.web_app_data.data)
+    print(data)
+    await message.answer(f'<b>{data["title"]}</b>\n\n<code>{data["desc"]}</code>\n\n{data["text"]}',
+                         parse_mode=ParseMode.HTML)
+
+def create_btn():
+    # Инициализируем ссылку на сайт
+    webAppInfo = types.WebAppInfo(url=url)
+    kb = [
+            [
+                types.KeyboardButton(text="Открыть приложение", web_app=webAppInfo),
+                types.KeyboardButton(text="О проекте")
+            ],
+         ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберете действие"
+    )
+    return keyboard
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+
+    if not mquery('create_tables'):
+        print(f'{BC.FAIL}Error: не удалось проверить целостность таблиц БД!{BC.ENDC}')
+    else:
+        asyncio.run(main())
---
+++ Dev/mquery.py
@@ -0,0 +1,110 @@
+import os
+from dotenv import load_dotenv
+import sqlite3
+import pandas as pd
+import yaml
+
+
+class BC:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+yaml_file = '../SQL/requests.yml'
+
+def mquery(sql_query: str, params=None):
+    """
+       В качестве входного sql_query могут быть переданы 3 типа параметров:
+            a) Метка для файла ../SQL/requests.yml
+            b) Путь к текстовому файлу
+            c) Непосредственно текст SQL запроса
+
+       1. Проверяем, является ли sql_query путем к файлу. Если да, то открываем и текст сохраняем в переменную sql_query.
+       2. Проверяем является ли текст переменной sql_query SQL запросом.
+       3. Если с текстом запроса передаются параметры, то проверяем количество параметров = кол-ву переменных
+       4. Проверим отсутствие SQL инъекций в тексте параметров
+       5. Если п.2 = п.3 = п.4 = True - выполняем текст запроса sql_query
+       """
+    load_dotenv()
+    db_path = os.getenv('db_path')
+
+    sql_keywords = ["select", "insert", "update", "delete", "from", "where", "join", "group by", "having", "order by",
+                    "create", "alter", "drop"]
+
+    with open(yaml_file, 'r') as file:
+        requests = yaml.safe_load(file)
+    #     Проверяем, является ли sql_query ключом Yaml
+    try:
+        sql_query = requests[sql_query]
+    except KeyError:
+        #     Проверяем, является ли sql_query файлом sql
+        if os.path.isfile(sql_query):
+            try:
+                with open(sql_query, 'r') as file:
+                    sql_query = file.read()
+            except (OSError, IOError):
+                print(f'{BC.FAIL}Error: не удалось открыть файл {BC.WARNING}{sql_query}.{BC.ENDC}')
+                return False
+
+    #   Проверим что полученный текст запроса это SQL запрос
+    if not any(keyword in sql_query for keyword in sql_keywords):
+        print(f'{BC.FAIL}Error: текст {BC.WARNING}{sql_query}{BC.FAIL} не является SQL запросом.{BC.ENDC}')
+        return False
+
+    #   Проверим наличие SQL инъекций в тексте передаваемых параметров
+    if params is None:
+        params = []
+    else:
+        for param in params:
+            if any(keyword in str(param) for keyword in sql_keywords):
+                print(f'{BC.FAIL}Error: текст переменной  {BC.WARNING}{param}{BC.FAIL} содержит SQL инъекцию!{BC.ENDC}')
+                return False
+
+    var_count = sql_query.count('%var%')
+    if var_count == len(params) > 0:
+        for i, value in enumerate(params):
+            sql_query = sql_query.replace(f'%var%', str(value), 1)
+    elif var_count > 0:
+        print(f'{BC.FAIL}Error: в запрос передается параметров {BC.WARNING}{len(params)}{BC.FAIL}'
+              f', ожидается {BC.WARNING}{var_count}{BC.ENDC}: \n{params}\n {sql_query}')
+        return False
+
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    out = False
+    try:
+        if 'select' in sql_query.lower():
+            out = pd.read_sql_query(sql_query, connection)
+        else:
+            cursor.executescript(sql_query)
+            connection.commit()
+            out = True
+        connection.close()
+    except Exception as e:
+        print(f'Не удалось выполнить запрос - {e}')
+    return out
+
+
+if __name__ == "__main__":
+    res = mquery('create_tables')
+    print(res)
+    print('!!!!!!!')
+    # res = mquery('../SQL/create tables.sql')
+    # print(res)
+    # params = [140291166, 'Dmitry_Ufimtsev', 'Дмитрий', 'Уфимцев']
+    # res = mquery('../SQL/add users.sql', params)
+    # print(res)
+    # params = [140291166]
+    # params = [140291166, 'Dmitry_Ufimtsev', 'Дмитрий', 'Уфимцев']
+    # res = mquery('add_users', params)
+    # print(res)
+    params = [140291166]
+    res = mquery('check_users', params)
+    print(res)
