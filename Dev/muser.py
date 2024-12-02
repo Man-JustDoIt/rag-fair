6813cde387954a62b01067a88c58ca4417420f8c
Day Vinn <dvudimtsev@gmail.com>
2024-12-02 07:36:36 +0300

    закончил тесты по правам пользователя

--- Dev/muser.py
+++ Dev/muser.py
@@ -1,9 +1,9 @@
 # todo В этом модуле соберем все операции с пользователями:
-# todo 1. Наличие пользователя в БД и его права, если он есть в БД - по умолчанию user  --  done
-# todo 2. Добавить пользователя в БД, если его нет в БД
-# todo 3. Изменить права пользователя
-# todo 4. Изменить данные пользователя
-# todo 5. Занести событие в log
+#       1. Наличие пользователя в БД и его права, если он есть в БД - по умолчанию user  --  done
+#       2. Добавить пользователя в БД, если его нет в БД
+#       3. Изменить данные пользователя
+#       4. Изменить права пользователя
+# todo  5. Занести событие в log
 
 import pandas as pd
 import numbers
@@ -33,19 +33,22 @@ def set_user_rights(tg_id, new_role, author_tg_id):
     res = check_user_and_rights(tg_id)
     if not res:
         return False
-    current_login, current_role = res[0], res[1]
+    current_login, current_role = res['tg_login'], res['role']
     if current_role == new_role:
         return 0
 
     res = check_user_and_rights(author_tg_id)
-    author_login, author_role = res[0], res[1]
+    author_login, author_role = res['tg_login'], res['role']
     if not author_role:
         return False
     if author_role != 'admin':
         print(f'{BC.FAIL}Error:{BC.ENDC} У пользователя {BC.WARNING}{author_role}{BC.ENDC} недостаточно прав!')
         return False
-    params = [tg_id, author_tg_id, new_role, tg_id]
-    res = mquery('set_user_and_rights', params)
+    if new_role == 'user':
+        res = mquery(f'update user_role_h set is_actual = 0 where tg_id = {tg_id}')
+    else:
+        params = [tg_id, author_tg_id, new_role, tg_id]
+        res = mquery('set_user_and_rights', params)
     if isinstance(res, numbers.Number) and res == 1:
         return res
     else:
@@ -84,13 +87,12 @@ def add_or_update_user(**kwargs):
         print(f'{BC.FAIL}Error:{BC.ENDC} Не хватает параметра {BC.WARNING}{delta}{BC.ENDC}!')
         return False
 
-    res = check_user_and_rights(tg_id, all=True)
+    res = check_user_and_rights(kwargs['tg_id'], all=True)
     if isinstance(res, dict) and len(res) > 0:  # Существующий пользователь
         res_dict = {k: res[k] for k in table_columns if k in res}
         if res_dict == kwargs:
             return 0
         else:
-            print(kwargs['tg_id'])
             res = mquery('deactivate_user_account', [kwargs['tg_id']])
             deactivate = 1
 
@@ -100,45 +102,39 @@ def add_or_update_user(**kwargs):
         params = [params_str, values_srt]
         res = mquery('add_new_user', params)
         if isinstance(res, numbers.Number) and res == 1:
-            return res
+            return res + deactivate
         else:
             return False
     else:
         return False
 
 
+def add_event2log():
+    #     todo  Функция добавляет в таблицу events_h информацию по следующим событиям:
+    #     todo  0. Регистрация нового пользователя в БД
+    #     todo  1. Вход пользователя с правами - нажатие кнопки 'команда /start'
+    table_name = 'user_events_h'
+#     нужно создать таблицу user_events_h со следующими полями:
+#   id - автоинкримент, уникальный идентификатор
+#   report_dt - дата и время события
+#   last_time_dt - дата и время события предыдущего события по этому пользователю
+#   tg_id - id пользователя
+#   tg_login - login пользователя
+#   role - role пользователя
+#   id_event - id события из словаря event_dict
+#   is_success - 0 или 1, успешно или неуспешно произошло событие
+#   comment - комментарий по событию
+
+
+
+
 if __name__ == "__main__":
-    tg_id = 8007893303
-    author_tg_id = 140291166
-    tg_login = 'UnderTheBrend_Bot'
-    first_name = 'ProBot'
+    test_user = {'tg_id': 123456, 'tg_login': 'test_tg_login', 'first_name': 'test_first_name',
+                 'last_name': 'test_last_name', 'phone': '+7 913 913 88 99',
+                 'corp_email': 'test_user@sber.ru', 'home_email': 'test_user@gmail.com'}
+    mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+    mquery(f"delete from user_role_h where tg_id = {test_user['tg_id']}")
 
-    # user = {'tg_id': 8007893303, 'tg_login': 'UnderTheBrend_Bot', 'first_name': 'ProBot', 'last_name': 'MIF'}
-    user = {'tg_id': 8007893303, 'first_name': 'ProBot', 'tg_login': 'UnderTheBrend_Bots'}
-    # user = {'tg_id': 8007893303, 'tg_login': 'UnderTheBrend_Bot'}
-    # # user = {'tg_id': 8007893303}
-    add_or_update_user(**user)
-
-    # res = check_user_and_rights(tg_id)
-    # print(res)
-
-    # params = [tg_id, tg_login, first_name]
-    # res = add_new_user(params)
-    # print(res)
-
-    # params = [tg_id, tg_login, first_name, last_name]
-
-    # print(check_user_and_rights(140291166))
-    # print(check_user_and_rights(123456))
-    # print(check_user_and_rights(1234567))
-
-    # res = set_user_rights(tg_id, 'admin', author_tg_id)
-    # print(res)
-    #
-    # res = check_user_and_rights(tg_id)
-    # print(res)
-
-    # print(set_user_rights(123456, 'admin', 140291166))
-    # print(set_user_rights(123456, 'content_redactor', 140291166))
-
-#     8007893303 bot tg_id
+    add_or_update_user(**test_user)
+    res = set_user_rights(test_user['tg_id'], 'moderator', 140291166)
+    print(res)
--- Tests/test_check_users.py
+++ Tests/test_check_users.py
@@ -3,69 +3,196 @@ import unittest
 import pandas as pd
 sys.path.append('../Dev/')
 
+import muser as mu
 from mquery import mquery
 
 class CheckUser(unittest.TestCase):
 
     # todo  Создать следующие тесты:
-    # todo  1. Первичная проверка:
-    # todo      1.1. Новый пользователь, в БД добавлен
-    # todo      1.2. Существующий в БД пользователь
-    # todo  2. Проверка прав пользователя:
-    # todo      2.1. Обычный пользователь
-    # todo      2.2. Админ
-    # todo      2.3. Редактор контента
-    # todo      2.4. Banned user
+    #       0. Пользователя нет в БД
+    #       1. Первичная проверка:
+    #           1.1. Новый пользователь, в БД добавлен
+    #           1.2. В БД уже есть такой пользователь - данные идентичны
+    #           1.3. В БД уже есть такой пользователь - данные не идентичны - обновить
+    #           1.4. Недостаточно данных для обновления
+    #       2. Получение/установка прав пользователя
+    #           2.1. Получение прав пользователя по умолчанию - 'user'
+    #           2.2. Назначение администраторам роли пользователя: 'moderator'
+    #           2.3. Назначение администраторам роли пользователя: 'user'
+    #           2.4. Изменение прав пользователя с 'user' на 'moderator'
+    #           2.5. Недостаточно прав для изменения роли
     # todo  3. Дате/время последнего входа
     # todo      3.1. В БД нет данных о входе
     # todo      2.2. В БД еть данные о входе
 
-    # 1.1. Новый пользователь, в БД добавлен
-    def test_add_new_user(self):
-        test_user = [123456, 'test_tg_login', 'test_first_name', 'test_last_name']
-        result = ''
+    # 0. Пользователя нет в БД
+    def test_user_not_in_bd(self):
+        # Подготовка
+        test_user = {'tg_id': 123456, 'tg_login': 'test_tg_login', 'first_name': 'test_first_name',
+                     'last_name': 'test_last_name', 'phone': '+7 913 913 88 99',
+                     'corp_email': 'test_user@sber.ru', 'home_email': 'test_user@gmail.com'}
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Исполнение
+        res = mu.check_user_and_rights(test_user['tg_id'])
+        # Результат
+        self.assertEqual(res, None)
 
-        res = mquery('check_user_in_BD', [test_user[0]])
-        if isinstance(res, pd.DataFrame) and res.empty:
-            mquery('add_new_user', test_user)
-            res = mquery('check_user_in_BD', [test_user[0]])
-            len_res = len(res)
-            tg_login = res['tg_login'].iloc(0)[0]
-            result = [len_res, tg_login]
-            sql_query = 'delete from user_accounts_h where tg_id = 123456'
-            mquery(sql_query, [test_user[0]])
+    # 1.1 Новый пользователь, в БД добавлен
+    def test_add_user(self):
+        # Подготовка
+        test_user = {'tg_id': 123456, 'tg_login': 'test_tg_login', 'first_name': 'test_first_name',
+                     'last_name': 'test_last_name', 'phone': '+7 913 913 88 99',
+                     'corp_email': 'test_user@sber.ru', 'home_email': 'test_user@gmail.com'}
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Исполнение
+        mu.add_or_update_user(**test_user)
+        res = mu.check_user_and_rights(test_user['tg_id'])
+        # Зачистка
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Результат
+        self.assertEqual(res['role'], 'user')
 
-        self.assertEqual(result, [1, 'test_tg_login'])
+    # 1.2. В БД уже есть такой пользователь - данные идентичны
+    def test_exist_equal_user(self):
+        # Подготовка
+        test_user = {'tg_id': 123456, 'tg_login': 'test_tg_login', 'first_name': 'test_first_name',
+                     'last_name': 'test_last_name', 'phone': '+7 913 913 88 99',
+                     'corp_email': 'test_user@sber.ru', 'home_email': 'test_user@gmail.com'}
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Исполнение
+        mu.add_or_update_user(**test_user)
+        res = mu.add_or_update_user(**test_user)
+        # Зачистка
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Результат
+        self.assertEqual(res, 0)
 
-    # 1.2. Существующий в БД пользователь
-    def test_user_already_exist(self):
-        res = mquery('check_user_in_BD', [140291166])
-        result = res['tg_login'].iloc(0)[0]
-        self.assertEqual(result, 'Dmitry_Ufimtsev')
+    # 1.3. В БД уже есть такой пользователь - данные не идентичны - обновить
+    def test_update_user_params(self):
+        # Подготовка
+        test_user = {'tg_id': 123456, 'tg_login': 'test_tg_login', 'first_name': 'test_first_name',
+                     'last_name': 'test_last_name', 'phone': '+7 913 913 88 99',
+                     'corp_email': 'test_user@sber.ru', 'home_email': 'test_user@gmail.com'}
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Исполнение
+        mu.add_or_update_user(**test_user)
+        test_user['last_name'] = 'test_last_names'
+        del test_user['home_email']
+        res = mu.add_or_update_user(**test_user)
+        # Зачистка
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Результат
+        self.assertEqual(res, 2)
 
-    # 2. Проверка прав пользователя:
-    def test_user_rights(self):
+    # 1.4. Недостаточно данных для обновления
+    def test_not_enough_params(self):
+        # Подготовка
+        test_user = {'tg_id': 123456, 'tg_login': 'test_tg_login', 'first_name': 'test_first_name',
+                     'last_name': 'test_last_name', 'phone': '+7 913 913 88 99',
+                     'corp_email': 'test_user@sber.ru', 'home_email': 'test_user@gmail.com'}
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Исполнение
+        mu.add_or_update_user(**test_user)
+        test_user['last_name'] = 'test_last_names'
+        del test_user['tg_login']
+        # Зачистка
+        res = mu.add_or_update_user(**test_user)
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Результат
+        self.assertEqual(res, False)
 
-        # создаем тестового пользователя
-        # не добавляем ему роль - роль должна быть 'user'
-        # проверяем является ли инициирующий пользователь администратором
-        # добавляем ему роль 'content_redactor' - проверяем, что роль присвоилась
-        # добавляем ему роль 'admin' - проверяем, что роль 'content_redactor' стала не активной, активна роль 'admin'
-        # добавляем ему роль 'banned_user' - проверяем, что все остальные роли не активны
-        # записываем 4 состояния пользователя
-        # удаляем пользователя и его роли
-        # сверяем результат тестирования с эталонным [123456, 'user', 'content_redactor', 'admin', 'banned_user']
+    # 2.1. Получение прав пользователя по умолчанию - user
+    def test_get_ordinary_user_rights(self):
+        # Подготовка
+        test_user = {'tg_id': 123456, 'tg_login': 'test_tg_login', 'first_name': 'test_first_name',
+                     'last_name': 'test_last_name', 'phone': '+7 913 913 88 99',
+                     'corp_email': 'test_user@sber.ru', 'home_email': 'test_user@gmail.com'}
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Исполнение
+        mu.add_or_update_user(**test_user)
+        res = mu.check_user_and_rights(test_user['tg_id'])
+        # Зачистка
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Результат
+        self.assertEqual(res['role'], 'user')
 
-        result = []
-        test_user = [123456, 'test_tg_login', 'test_first_name', 'test_last_name']
-        res = mquery('check_user_in_BD', [test_user[0]])
-        if isinstance(res, pd.DataFrame) and res.empty:
-            mquery('add_new_user', test_user)
-        # роль - user
-        res = mquery('check_user_and_rights', [test_user[0]])
-        result.append(res['role'].iloc(0)[0])
+    # 2.2. Назначение администраторам роли пользователя: moderator
+    def test_set_moderator_role(self):
+        # Подготовка
+        test_user = {'tg_id': 123456, 'tg_login': 'test_tg_login', 'first_name': 'test_first_name',
+                     'last_name': 'test_last_name', 'phone': '+7 913 913 88 99',
+                     'corp_email': 'test_user@sber.ru', 'home_email': 'test_user@gmail.com'}
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        mquery(f"delete from user_role_h where tg_id = {test_user['tg_id']}")
+        # Исполнение
+        mu.add_or_update_user(**test_user)
+        mu.set_user_rights(test_user['tg_id'], 'moderator', 140291166)
+        res = mu.check_user_and_rights(test_user['tg_id'])
+        # Зачистка
+        mquery(f"delete from user_role_h where tg_id = {test_user['tg_id']}")
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Результат
+        self.assertEqual(res['role'], 'moderator')
 
-        self.assertEqual(result, ['user'])
+    # 2.3. Назначение администраторам роли пользователя: 'user'
+    def test_set_user_role_1(self):
+        # Подготовка
+        test_user = {'tg_id': 123456, 'tg_login': 'test_tg_login', 'first_name': 'test_first_name',
+                     'last_name': 'test_last_name', 'phone': '+7 913 913 88 99',
+                     'corp_email': 'test_user@sber.ru', 'home_email': 'test_user@gmail.com'}
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        mquery(f"delete from user_role_h where tg_id = {test_user['tg_id']}")
+        # Исполнение
+        mu.add_or_update_user(**test_user)
+        mu.set_user_rights(test_user['tg_id'], 'user', 140291166)
+        res = mu.check_user_and_rights(test_user['tg_id'])
+        # Зачистка
+        mquery(f"delete from user_role_h where tg_id = {test_user['tg_id']}")
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Результат
+        self.assertEqual(res['role'], 'user')
+
+    # 2.4. Изменение прав пользователя с 'user' на 'moderator'
+    def test_set_user_role_2(self):
+        # Подготовка
+        test_user = {'tg_id': 123456, 'tg_login': 'test_tg_login', 'first_name': 'test_first_name',
+                     'last_name': 'test_last_name', 'phone': '+7 913 913 88 99',
+                     'corp_email': 'test_user@sber.ru', 'home_email': 'test_user@gmail.com'}
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        mquery(f"delete from user_role_h where tg_id = {test_user['tg_id']}")
+        # Исполнение
+        mu.add_or_update_user(**test_user)
+        mu.set_user_rights(test_user['tg_id'], 'moderator', 140291166)
+        mu.set_user_rights(test_user['tg_id'], 'user', 140291166)
+
+        res = mu.check_user_and_rights(test_user['tg_id'])
+        # Зачистка
+        mquery(f"delete from user_role_h where tg_id = {test_user['tg_id']}")
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Результат
+        self.assertEqual(res['role'], 'user')
+
+# 2.5. Недостаточно прав для изменения роли
+    def test_set_user_role_3(self):
+        # Подготовка
+        test_user = {'tg_id': 123456, 'tg_login': 'test_tg_login', 'first_name': 'test_first_name',
+                     'last_name': 'test_last_name', 'phone': '+7 913 913 88 99',
+                     'corp_email': 'test_user@sber.ru', 'home_email': 'test_user@gmail.com'}
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        mquery(f"delete from user_role_h where tg_id = {test_user['tg_id']}")
+        # Исполнение
+        mu.add_or_update_user(**test_user)
+        mu.set_user_rights(test_user['tg_id'], 'moderator', test_user['tg_id'])
+        res = mu.check_user_and_rights(test_user['tg_id'])
+
+        # Зачистка
+        mquery(f"delete from user_role_h where tg_id = {test_user['tg_id']}")
+        mquery(f"delete from user_accounts_h where tg_id = {test_user['tg_id']}")
+        # Результат
+        self.assertEqual(res['role'], 'user')
 
 
 
+
+if __name__ == '__main__':
+    unittest.main()
